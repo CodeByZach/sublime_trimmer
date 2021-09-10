d5e0c4f6097c03b426c5a92a247756a69a54389f
Zachary Miller <codebyzach@gmail.com>
2021-09-10 15:19:07 -0400

    GitHub Workflow compatibility

--- utils.py
+++ utils.py
@@ -1,10 +1,14 @@
+"""Utility Class."""
 import sublime
 import sublime_plugin
 import os
 
 
 class ZEditSettings(sublime_plugin.WindowCommand):
+    """For editing package settings."""
+
     def run(self, **kwargs):
+        """Default method."""
 
         if 'is_parent_setting' in kwargs and kwargs.get('is_parent_setting'):
             del kwargs['is_parent_setting']
@@ -12,14 +16,22 @@ class ZEditSettings(sublime_plugin.WindowCommand):
                 kwargs['file'] = kwargs.get('base_file')
                 del kwargs['base_file']
 
-            expanded_target_directory = sublime.expand_variables(os.path.dirname(kwargs.get('file')), self.window.extract_variables())
+            expanded_target_directory = sublime.expand_variables(
+                os.path.dirname(kwargs.get('file')),
+                self.window.extract_variables()
+            )
             if not os.path.exists(expanded_target_directory):
                 os.makedirs(expanded_target_directory)
 
             try:
-                expanded_source_path = sublime.expand_variables(kwargs.get('file'), self.window.extract_variables())
-                # package_name = os.path.basename(os.path.dirname(expanded_source_path.replace(sublime.packages_path(), '')))
-                resource_path = 'Packages'+expanded_source_path.replace(sublime.packages_path(), '')
+                expanded_source_path = sublime.expand_variables(
+                    kwargs.get('file'),
+                    self.window.extract_variables()
+                )
+                # package_name = os.path.basename(
+                #   os.path.dirname(expanded_source_path.replace(sublime.packages_path(), ''))
+                # )
+                resource_path = 'Packages' + expanded_source_path.replace(sublime.packages_path(), '')
                 sublime.load_resource(resource_path)
                 if 'default' in kwargs:
                     del kwargs['default']
@@ -32,14 +44,22 @@ class ZEditSettings(sublime_plugin.WindowCommand):
 
         else:
             if 'user_file' in kwargs:
-                expanded_target_directory = sublime.expand_variables(os.path.dirname(kwargs.get('user_file')), self.window.extract_variables())
+                expanded_target_directory = sublime.expand_variables(
+                    os.path.dirname(kwargs.get('user_file')),
+                    self.window.extract_variables()
+                )
                 if not os.path.exists(expanded_target_directory):
                     os.makedirs(expanded_target_directory)
 
             try:
-                expanded_source_path = sublime.expand_variables(kwargs.get('base_file'), self.window.extract_variables())
-                # package_name = os.path.basename(os.path.dirname(expanded_source_path.replace(sublime.packages_path(), '')))
-                resource_path = 'Packages'+expanded_source_path.replace(sublime.packages_path(), '')
+                expanded_source_path = sublime.expand_variables(
+                    kwargs.get('base_file'),
+                    self.window.extract_variables()
+                )
+                # package_name = os.path.basename(
+                #   os.path.dirname(expanded_source_path.replace(sublime.packages_path(), ''))
+                # )
+                resource_path = 'Packages' + expanded_source_path.replace(sublime.packages_path(), '')
                 sublime.load_resource(resource_path)
 
                 self.window.run_command('edit_settings', kwargs)
@@ -48,9 +68,19 @@ class ZEditSettings(sublime_plugin.WindowCommand):
                     kwargs['file'] = kwargs.get('user_file')
                     del kwargs['user_file']
                 else:
-                    # expanded_source_path = sublime.expand_variables(kwargs.get('base_file'), self.window.extract_variables())
-                    # kwargs['file'] = expanded_source_path.replace(sublime.packages_path(), os.path.join(sublime.packages_path(), 'User'))
-                    kwargs['file'] = os.path.join(sublime.packages_path(), 'User', os.path.basename(kwargs.get('base_file')))
+                    # expanded_source_path = sublime.expand_variables(
+                    #   kwargs.get('base_file'),
+                    #   self.window.extract_variables()
+                    # )
+                    # kwargs['file'] = expanded_source_path.replace(
+                    #   sublime.packages_path(),
+                    #   os.path.join(sublime.packages_path(), 'User')
+                    # )
+                    kwargs['file'] = os.path.join(
+                        sublime.packages_path(),
+                        'User',
+                        os.path.basename(kwargs.get('base_file'))
+                    )
                 del kwargs['base_file']
                 if 'default' in kwargs:
                     kwargs['contents'] = kwargs.get('default')
